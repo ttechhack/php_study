4fc145a95e0bc4051ac77a3a9b2529f9b488b605
akihiro0710 <acedia2074@gmail.com>
2017-01-17 15:48:46 +0900

    フォームを作成

--- index.php
+++ index.php
@@ -5,6 +5,9 @@
   <title>PHP study</title>
 </head>
 <body>
-
+<form action="" method="GET">
+  <input type="text" name="val">
+  <button type="submit" name="submit" value="submit">送信</button>
+</form>
 </body>
 </html>
